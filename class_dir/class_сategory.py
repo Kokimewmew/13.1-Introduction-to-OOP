d6de4f9d388b4d469752ee8bcdac2ec69948ef27
Artur <kokivirivoki@mail.ru>
2024-03-12 06:54:41 +0500

    Home work 14.1 Change

--- class_dir/class_сategory.py
+++ class_dir/class_сategory.py
@@ -2,7 +2,7 @@ from class_dir.class_product import Product
 
 
 class Category:
-    title: str
+    name: str
     description: str
     goods: list
     count_category = 0  # Счетчик общего количества категорий
@@ -16,29 +16,35 @@ class Category:
         self.__goods = []  # Список товаров сделали приватным
         Category.count_category += 1  # при создании экземлпяра, счетчик увеличивается на 1
 
+    @property
+    def goods(self):
+        '''Данный метод позволяет принтом вывести приватный список товаров'''
+        return self.__goods
+
     def append_goods(self, good):
         '''
          Метод добавления товаров в список
         также данный метод считает итоговое количество всех продуктов в сумме
         '''
         for item in self.__goods:
-            if item[0] == good.name:  # Проверяем наименование товара
-                item[3] += good.quantity  # Обновляем количество товаров
+            if item[0] == good.name:  # Проверяем наименование товара есть ли в списке продуктов
+                item[3] += good.quantity  # Обновляем количество товаров в продукте который уже есть
                 break
         else:
-            self.__goods.append([good.name, good.description, good.price, good.quantity])
+            self.__goods.append([good.name, good.description, good.price, good.quantity])  # Добавляем новый продукт
             Category.unique_product += 1
 
     @property
-    def display(self):
-        '''Данный метод позволяет принтом вывести приватный список товаров'''
-        return self.__goods
-
-    @property
     def counting_goods(self):
-        return len(self.__goods)
+        count = 0
+        for i in self.__goods:
+            count += i[3]
+
+        return count
+
 
     def __repr__(self):
+        '''Метод отображения экземпляра категории'''
         return f'{self.name}, {self.description}, {self.__goods}'
 
     @property
@@ -51,11 +57,8 @@ class Category:
 
     def __len__(self):
         '''Вывод количества продуктов на складе'''
-        count = 0
-        for i in self.__goods:
-            count += i[3]
+        return len(self.__goods)
 
-        return count
 
     def __str__(self):
         '''Добавлено строковое отображение'''
@@ -64,20 +67,21 @@ class Category:
 
 if __name__ == '__main__':
     category_1 = Category('Телефоны', 'мобильные телефоны')  # Экземпляр класса Category
-
     product_1 = Product('Samsung', 'smth', 90_000, 2)  # Экземпляр класса Product
     product_2 = Product('iPhone', 'smth', 100_000, 3)  # Экземпляр класса Product
 
     category_1.append_goods(product_1)  # Добавление продукта в приватный список товаров
     category_1.append_goods(product_2)  # Добавление продукта в приватный список товаров
-    print(category_1.display)
+    print(category_1.goods)
 
     product_3 = Product('iPhone', 'smth', 100_000, 34)
     category_1.append_goods(product_3)  # Добавление продукта в приватный список товаров если такой продукт существует
-    print(category_1.display)  # Отображение приватного списка товаров
+    print(category_1.goods)  # Отображение приватного списка товаров
 
     print(f'Уникальных продуктов: {category_1.unique_product}')  # Количество уникальных товаров в приватном списке
     print(category_1.get_format)  # Получение перечня товаров определенным форматом
     print(str(category_1))  # Отображение строкового представления
 
-    print(f'Вывод  общего количества продуктов категории: {len(category_1)}')
+    print(f'Вывод  общего количества продуктов категории: {category_1.counting_goods}')
+
+
---
+++ tests/test_class_category.py
@@ -0,0 +1,72 @@
+import pytest
+from class_dir.class_product import Product
+from class_dir.class_сategory import Category
+
+
+@pytest.fixture
+def new_category():
+    category_1 = Category('Телефоны', 'мобильные телефоны')
+    return category_1
+
+
+@pytest.fixture
+def new_good():
+    product_1 = Product('Samsung', 'smth', 90_000, 2)
+    return product_1
+
+
+def test_goods(new_category):
+    assert new_category.goods == []
+
+
+def test_append_goods(new_category, new_good):
+    """Тест на добавление продукта в экземпляр"""
+    new_category.append_goods(new_good)
+    assert new_category.name == 'Телефоны'
+    assert new_category.description == 'мобильные телефоны'
+    assert new_category.goods == [['Samsung', 'smth', 90_000, 2]]
+
+    product_2 = Product('iPhone', 'smth', 100_000, 3)
+    new_category.append_goods(product_2)
+    assert new_category.goods == [['Samsung', 'smth', 90_000, 2], ['iPhone', 'smth', 100_000, 3]]
+
+
+def test_counting_goods(new_category, new_good):
+    """Тест на подсчет общего количества товаров"""
+    assert new_category.counting_goods == 0
+    new_category.append_goods(new_good)
+    assert new_category.counting_goods == 2
+    product_2 = Product('iPhone', 'smth', 100_000, 3)
+    new_category.append_goods(product_2)
+    assert new_category.counting_goods == 5
+
+
+def test_get_format(new_category, new_good):
+    """Тест на получение перечня товаров определенным форматом"""
+    new_category.append_goods(new_good)
+    assert new_category.get_format == 'Samsung, 90000 руб. Остаток: 2 шт.\n'
+
+
+def test_len(new_category, new_good):
+    """Тест на количество продуктов в категории"""
+    new_category.append_goods(new_good)
+    assert len(new_category) == 1
+    product_2 = Product('iPhone', 'smth', 100_000, 3)
+    new_category.append_goods(product_2)
+    assert len(new_category) == 2
+
+
+def test_str(new_category, new_good):
+    """Тест на Отображение строкового представления"""
+    new_category.append_goods(new_good)
+    assert str(new_category) == "Название категории: Телефоны, количество продуктов: 1 шт."
+
+
+def test_repr(new_category, new_good):
+    """Тест на отображения экземпляра категории"""
+    new_category.append_goods(new_good)
+    assert repr(new_category) == "Телефоны, мобильные телефоны, [['Samsung', 'smth', 90000, 2]]"
+    product_2 = Product('iPhone', 'smth', 100_000, 3)
+    new_category.append_goods(product_2)
+    assert repr(new_category) == "Телефоны, мобильные телефоны, [['Samsung', 'smth', 90000, 2], ['iPhone', 'smth', 100000, 3]]"
+
